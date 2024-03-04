4e152f81190cf8122140c14c36cd170780106e33
tep-vila <vila.sj.bscs@gmail.com>
2024-03-04 18:11:08 +0800

    working ui and  game logic. no scoring yet

--- RPS.js
+++ RPS.js
@@ -1,13 +1,30 @@
-// gawa ng function computerSelection pang generate ng sagot ng computer
-// function userSelection na pang take ng sagot ng user
-// variables na nag represent sa RPS
-// pang process ng logic sino nanalo
-// loop function playGame ng game para sa scoring and winner identifier
+const winLoseMsg = document.querySelector(".win-lose-msg");
+const playerScore = document.querySelector(".curr-player-score");
+const compScore = document.querySelector(".curr-comp-score");
+const compChoiceDisplay = document.querySelector(".comp-choice-img");
+const playerChoiceLbl = document.querySelector(".player-choice");
+const compChoiceLbl = document.querySelector(".comp-choice");
+const optionRow = document.querySelectorAll(".choose-option div");
+const choiceDisplay = document.querySelector(".choice-display");
+const resetChoice =
+  '<span class="player-choice">YOU</span> VS <span class="comp-choice">COMP</span>';
 
-const generateChoice = () => (choice = Math.ceil(Math.random() * 3));
+const defaultScore = "00";
+
+optionRow.forEach((choices) => {
+  choices.addEventListener("click", (event) => {
+    let choice = event.currentTarget.className;
+    choice.toString();
+    playRound(computerSelection(), choice);
+  });
+});
 
 function computerSelection() {
-  let choice = generateChoice();
+  let choice = () => {
+    return (choice = Math.ceil(Math.random() * 3));
+  };
+
+  choice();
 
   switch (choice) {
     case 1:
@@ -24,39 +41,42 @@ function computerSelection() {
   }
 }
 
-const caseInsensitive_er = (choice) => choice.toLowerCase();
-
-const answerIsValid = (choice) =>
-  choice === ("rock" || "paper" || "scissors") ? true : false;
-
 function playRound(computerSelection, playerSelection) {
   if (computerSelection === playerSelection) {
     console.log("it's a tie");
+    tie(computerSelection);
   } else if (
     (computerSelection === "rock" && playerSelection === "scissors") ||
     (computerSelection === "paper" && playerSelection === "rock") ||
     (computerSelection === "scissors" && playerSelection === "paper")
   ) {
     console.log("You lose");
+    lose(computerSelection);
   } else {
     console.log("You win");
+    win(computerSelection);
   }
 }
 
-function mainLoop() {
-  let playerChoice;
-  let computerChoice;
+function tie(computerSelection) {
+  choiceDisplay.innerHTML = "Tie!";
+  updateCompImg(computerSelection);
+  setTimeout(() => (choiceDisplay.innerHTML = resetChoice), 2000);
+}
 
-  for (let i = 1; i <= 5; i++) {
-    playerChoice = caseInsensitive_er(prompt("rock, paper, or scissors?"));
-    computerChoice = computerSelection();
+function updateCompImg(compChoice) {
+  compChoiceDisplay.src = `assets/${compChoice}.png`;
+  console.log(compChoiceDisplay);
+}
 
-    if (answerIsValid(playerChoice)) {
-      playRound(computerChoice, playerChoice);
-    } else {
-      console.log("invalid answer");
-      console.log(i);
-      i -= 1;
-    }
-  }
+function lose(computerSelection) {
+  choiceDisplay.innerHTML = "Round lost!";
+  updateCompImg(computerSelection);
+  setTimeout(() => (choiceDisplay.innerHTML = resetChoice), 2000);
+}
+
+function win(computerSelection) {
+  choiceDisplay.innerHTML = "Round won!";
+  updateCompImg(computerSelection);
+  setTimeout(() => (choiceDisplay.innerHTML = resetChoice), 2000);
 }
